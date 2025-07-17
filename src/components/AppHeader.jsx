48352b65261631895d70ea92416ed0a57592df3f
Your Name <you@example.com>
2025-07-17 15:42:28 +0200

    Refactor App component and remove unused styles
    - Removed the existing styles from App.css and index.css.
    - Simplified App.jsx by removing the logo and count functionality.
    - Introduced a new AppHeader component for the header section.
    - Created a new AppMain component file for future use.

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+
+function AppHeader() {
+    return (
+        <header>
+            <h1 className="p-3 bg-light">Learn Web Development</h1>
+        </header>
+    );
+}
+
+export default AppHeader;
